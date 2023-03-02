80286f20a1face7b57f6b89a5a2107232e6e934b
Piippo Juuso Heikki Aleksanteri <juuso.piippo1@gmail.com>
2023-03-02 15:02:52 +0200

    Pokemonit luokka ja sailoexception

--- src/rekisteri/Pokemonit.java
+++ src/rekisteri/Pokemonit.java
@@ -10,12 +10,111 @@ package rekisteri;
  */
 public class Pokemonit {
 
+    private String tiedostoNimi;
+    private int lkm;
+    private int maxLkm = 8;
+    private Pokemon[] taulukko = new Pokemon[maxLkm];
+    
+    
+    /**
+     * Oletusmuodostaja
+     */
+    public Pokemonit() {
+        //
+    }
+
+    /**
+     * Lisaa pokemonin taulukkoon
+     * @param pokemon pokemon joka lisataan
+     * @throws SailoException poikkeus, joka heitetään jos taulukko täynnä
+     * @example
+     * <pre name="test">
+     *      #THROWS SailoException 
+     *      Pokemonit pokemonit = new Pokemonit();
+     *      Pokemon pikachu = new Pokemon(), charizard = new Pokemon();
+     *      pokemonit.getLkm() === 0;
+     *      pokemonit.lisaa(pikachu); pokemonit.getLkm() === 1;
+     *      pokemonit.lisaa(charizard); pokemonit.getLkm() === 2;
+     *      pokemonit.lisaa(pikachu); pokemonit.getLkm() === 3;
+     *      pokemonit.getPokemon(0) === pikachu;
+     *      pokemonit.getPokemon(1) === charizard;
+     *      pokemonit.getPokemon(2) === pikachu;
+     *      pokemonit.getPokemon(1) == pikachu === false;
+     *      pokemonit.getPokemon(1) == charizard === true;
+     *      pokemonit.getPokemon(3) === pikachu; #THROWS IndexOutOfBoundsException 
+     *      pokemonit.lisaa(pikachu); pokemonit.getLkm() === 4;
+     *      pokemonit.lisaa(pikachu); pokemonit.getLkm() === 5;
+     *      pokemonit.lisaa(pikachu); pokemonit.lisaa(pikachu);
+     *      pokemonit.lisaa(pikachu); 
+     *      pokemonit.lisaa(pikachu); #THROWS SailoException
+     * </pre>
+     */
+    public void lisaa(Pokemon pokemon) throws SailoException {
+        if (taulukko.length <= lkm)
+            throw new SailoException("Alkio ei mahdu taulukkoon");
+        taulukko[lkm] = pokemon;
+        lkm++;
+    }
+    
+    
+    /**
+     * Tallentaa jäsenistön tiedostoon.  Kesken.
+     * @throws SailoException jos talletus epäonnistuu
+     */
+    public void talleta() throws SailoException {
+        throw new SailoException("Ei osata vielä tallettaa tiedostoa " + tiedostoNimi);
+    }
+
+
+    /**
+     * @return taulukon alkioiden lkm
+     */
+    public int getLkm() {
+        return lkm;
+    }
+
+
+    /**
+     * @param i indeksi
+     * @return pokemon paikassa i
+     * @throws IndexOutOfBoundsException heitetään jos ei ole indeksissä pokemonia
+     * @example
+     */
+    public Pokemon getPokemon(int i) throws IndexOutOfBoundsException {
+        if (i < 0 || lkm <= i)
+            throw new IndexOutOfBoundsException("Laiton indeksi: " + i);
+        return taulukko[i];
+    }
+
+
     /**
      * @param args ei kayt.
      */
     public static void main(String[] args) {
-    // TODO Auto-generated method stub
-    
+        Pokemonit pokemonit = new Pokemonit();
+
+        Pokemon pikachu = new Pokemon(), charizard = new Pokemon();
+        pikachu.rekisteroi();
+        pikachu.vastaa_pikachu();
+        charizard.rekisteroi();
+        charizard.vastaa_pikachu();
+
+        try {
+            pokemonit.lisaa(pikachu);
+            pokemonit.lisaa(charizard);
+
+            System.out.println("============= Pokemonit testi =================");
+
+            for (int i = 0; i < pokemonit.getLkm(); i++) {
+                Pokemon Pokemon = pokemonit.getPokemon(i);
+                System.out.println("Pokemonin nro: " + i);
+                Pokemon.tulosta(System.out);
+                System.out.println();
+            }
+
+        } catch (SailoException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 
 }
---
+++ src/rekisteri/SailoException.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package rekisteri;
+
+/**
+ * @author Juuso Piippo & Elias Lehtinen
+ * @version 2.3.2023
+ *
+ */
+public class SailoException extends Exception {
+    private static final long serialVersionUID = 1L;
+
+
+    /**
+     * Poikkeuksen muodostaja jolle tuodaan poikkeuksessa
+     * käytettävä viesti
+     * @param viesti poikkeuksen viesti
+     */
+    public SailoException(String viesti) {
+        super(viesti);
+    }
+
+}
