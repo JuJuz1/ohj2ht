84fe4a22fe8035975c46409a26b8dff43da3bc44
Lehtinen Elias <elias.a.lehtinen@student.jyu.fi>
2023-04-13 12:26:56 +0300

    elementit.dat ja Elementit-luokkaan lueTiedostosta()

--- src/rekisteri/Elementit.java
+++ src/rekisteri/Elementit.java
@@ -3,8 +3,12 @@
  */
 package rekisteri;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Scanner;
 
 /**
  * Sisältää listan elementeistä
@@ -19,15 +23,25 @@ public class Elementit {
     
     // private int lkm;
     // private int maxLkm;
-    private String tiedostoNimi;
+    private String tiedostoNimi = "elementit.dat";
     private final Collection<Elementti> alkiot = new ArrayList<Elementti>();  // Lista elementeille
     
     
     /**
-     * Myöhemmin alkiot luetaan tiedostosta
+     * Alustetaan elementit-olio oletusarvoilla.
      */
     public Elementit() {
-        // Ei tarvita vielä
+        // tiedostoNimi oletusarvo = "elementit.dat"
+        // alkiot oletusarvo = new ArrayList<Elementti>()
+    }
+    
+    
+    /**
+     * Asettaa uuden tiedoston nimen
+     * @param tiedNimi Uusi tiedoston nimi
+     */
+    public Elementit(String tiedNimi) {
+        this.tiedostoNimi = tiedNimi;
     }
     
     
@@ -71,6 +85,46 @@ public class Elementit {
      */
     public int getLkm() {
         return alkiot.size();
+    }
+    
+    
+    /**
+     * @return tiedostoNimi
+     */
+    public String getTiedostoNimi() {
+        return this.tiedostoNimi;
+    }
+    
+    
+    /**
+     * Lukee elementit tiedostosta ja lisää ne alkiot-listaan.
+     * Jos tiedosto ei aukea tulostaa System.err-tietovirtaan "Tiedosto ei aukea." + virheen message.
+     * @example
+     * <pre name="test">
+     *   #THROWS IOException
+     *   #import java.io.IOException;
+     *   #import fi.jyu.mit.ohj2.VertaaTiedosto;
+     *   #import java.util.ArrayList;
+     *   String tiedosto = "elementitTest.dat";
+     *   VertaaTiedosto.tuhoaTiedosto(tiedosto);
+     *   VertaaTiedosto.kirjoitaTiedosto(tiedosto, ";id|elementti    |vahvuusID  |heikkousID\n 1 |vesi         |2          |6\n 2 |tuli         |4          |1\n 3 |maa          |5          |4");
+     *   Elementit el = new Elementit(tiedosto);
+     *   el.lueTiedostosta();
+     *   el.getLkm() === 3;
+     *   el.etsiElementti(1).getNimi() === "vesi";
+     *   el.etsiElementti(2).getVahvuusID() === 4;
+     *   el.etsiElementti(3).getHeikkousID() === 4;
+     * </pre>
+     */
+    public void lueTiedostosta() {
+        try (Scanner fi = new Scanner(new FileInputStream(new File(this.getTiedostoNimi())))){
+            while (fi.hasNext()) {
+                String rivi = fi.nextLine();
+                if (rivi.charAt(0) != ';') alkiot.add(new Elementti(rivi));
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("Tiedosto ei aukea." + e.getMessage());
+        }
     }
     
     
