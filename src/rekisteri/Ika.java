d0a8e86c30bdc2f12dd440089fae579bc116909b
Lehtinen Elias <elias.a.lehtinen@student.jyu.fi>
2023-04-13 11:50:54 +0300

    Ika-olion parse merkkijonosta

--- src/rekisteri/Ika.java
+++ src/rekisteri/Ika.java
@@ -3,6 +3,8 @@
  */
 package rekisteri;
 
+import fi.jyu.mit.ohj2.Mjonot;
+
 /**
  * Sisältää ID:n ja ikäalueen
  * Osaa kertoa ID:n ja ikäalueen
@@ -37,6 +39,41 @@ public class Ika {
     
     
     /**
+     * TODO: tee parempi käsittely jos jono väärän muotoinen, tällä hetkellä ika null
+     * Muodostaja.
+     * Luo uuden ikäryhmän merkkijonosta kutsumalla parse-metodia.
+     * Jos jono on väärän muotoinen (liikaa tai liian vähän kenttiä) alustetaan oletusarvoilla (tyhjillä)
+     * @param jono Merkkijono, joka sisältää iän tiedot muodossa  "id |ika"
+     * @example
+     * <pre name="test">
+     *   Ika i1 = new Ika("1|53-95");  Ika i2 = new Ika("   2  |33-23     ");  Ika i3 = new Ika("");
+     *   i1.getID() === 1;   i1.getIka() === "53-95";
+     *   i2.getID() === 2;   i2.getIka() === "33-23";
+     *   i3.getID() === 0;   i3.getIka() === null;
+     * </pre>
+     */
+    public Ika(String jono) {
+        this.parse(jono); // Palauttaa false ja ei tee muutoksia attribuuttien arvoihin, jos jono väärän muotoinen
+    }
+    
+    
+    /**
+     * Alustaa ikäryhmän attribuutit merkkijonosta luetuilla arvoilla.
+     * Jos kokonaislukua ei voida lukea, oletusarvo on -1.
+     * 
+     * @param jono Merkkijono, josta attribuuttien arvot luetaan.
+     * @return true, jos onnistui, false jos merkkijono on väärän muotoinen (liian lyhyt tai pitkä)
+     */
+    public boolean parse(String jono) {
+        String[] tiedot = jono.split(" *\\| *");
+        if (tiedot.length != 2) return false;
+        this.setID(Mjonot.erotaInt(tiedot[0], -1));
+        this.setIka(tiedot[1].trim());
+        return true;
+    }
+    
+    
+    /**
      * @return ID
      */
     public int getID() {
@@ -45,10 +82,28 @@ public class Ika {
     
     
     /**
+     * Asettaa ikäryhmän ID:n
+     * @param id Uusi ID:n arvo
+     */
+    public void setID(int id) {
+        this.ID = id;
+    }
+    
+    
+    /**
      * @return ikä-alue
      */
     public String getIka() {
         return ika;
+    }
+    
+    
+    /**
+     * Asettaa ika-parametrille uuden arvon
+     * @param ika Uusi ikä
+     */
+    public void setIka(String ika) {
+        this.ika = ika;
     }
     
     
